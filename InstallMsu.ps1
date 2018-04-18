96966ca92f2ec569d1acd60d064eb8bcd4457b67
bikramgcnz <38302920+bikramgcnz@users.noreply.github.com>
2018-04-18 14:15:50 +1200

    Add files via upload

---
+++ InstallMsu.ps1
@@ -0,0 +1,8 @@
+
+$dir = (Get-Item -Path ".\" -Verbose).FullName
+ Foreach($item in (ls $dir *.msu -Name))
+ {
+    echo $item
+    $item = $dir + "\" + $item
+    wusa $item /quiet /norestart | Out-Null
+ }
